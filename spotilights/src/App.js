d27e8c0d038a4832a433b34a2b20da4a257dd86a
Eric Nguyen <eriicnguyen1@gmail.com>
2024-11-03 18:40:24 -0800

    setting up react

---
+++ spotilights/src/App.js
@@ -0,0 +1,13 @@
+import SongCover from "./SongCover";
+import styles from "./App.module.css";
+
+function App(){
+  return (
+    <>
+      <div><SongCover/></div>
+      <h1 className={styles.h1}>hi</h1>
+    </>
+  );
+}
+
+export default App;
