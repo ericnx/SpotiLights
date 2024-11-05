74cc1f6fc1378ca59f430db9de304470091f016d
Eric Nguyen <eriicnguyen1@gmail.com>
2024-11-05 02:05:29 -0800

    4

--- spotilights/src/App.js
+++ spotilights/src/App.js
@@ -1,61 +1,16 @@
 import {Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import Title from './Title';
 import Login from './Login';
 
 export default function App() {
     return (
       <Container 
-        className='d-flex justify-content-center align-items-center flex-column'
-        style={{minHeight:'100vh'}}
+        className='d-flex justify-content-center align-items-center flex-column vh-100 text-center'
       >
-        <Title/>    
+        <h1><b>SpotiLights</b></h1> 
+        <p>Enjoy your music's lights.</p> 
         <Login/>
       </Container>
     )
-    // const [song, setSong] = useState(null);
-    // const [accessToken, setAccessToken] = useState('');
-
-    // useEffect(() => {
-    //     const urlParams = new URLSearchParams(window.location.search);
-    //     const token = urlParams.get('access_token');
-    //     console.log("Access token2: "+token);
-    //     if (token) {
-    //         setAccessToken(token);
-    //         fetchCurrentSong(token);
-    //         window.history.replaceState({}, document.title, "/");
-    //     }
-    //     else{
-    //       console.log("no token");
-    //     }
-    // }, []);
-
-    // const fetchCurrentSong = async (token) => {
-    //     try {
-    //         const response = await fetch(`http://localhost:3000/currently-playing?access_token=${token}`);
-    //         const data = await response.json();
-    //         setSong(data);
-    //     } catch (error) {
-    //         console.error('Error fetching current song:', error);
-    //     }
-    // };
-
-    // return (
-    //     <div className="container">
-    //         <h1 className={AppStyles.title}><b>SpotiLights</b></h1>
-    //         {accessToken ? 
-    //           (song ? (
-    //                 <div>
-    //                     <SongCover coverUrl={song.cover} />
-    //                     <SongDetails title={song.title} artist={song.artist} />
-    //                 </div>
-    //             ) : (
-    //                 <p>Loading song...</p>
-    //             )
-    //           ) : (
-    //             <LoginButton/>
-    //           )}
-    //     </div>
-    // );
 }
 
